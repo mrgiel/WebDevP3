12889c86b3db56e496aa43ace57e6741e1ac54df
mrgiel <47106654+mrgiel@users.noreply.github.com>
2022-02-28 14:33:09 +0100

    display db in /Search pagina

--- WebApplication1/Models/Uitgave.cs
+++ WebApplication1/Models/Uitgave.cs
@@ -1,1 +1,15 @@
-﻿
+﻿namespace WebApplication1.Models;
+public class Uitgave
+{
+    public int ISBN { get; set; }
+    public string Naam { get; set; }
+    public int versie_nr { get; set; }
+    public int jaar { get; set; }
+    public int hoogte { get; set; }
+    public string afbeeldingURL { get; set; }
+    public string uitgever { get; set; }
+    public int reeks_nummer { get; set; }
+    public int categorie_nummer { get; set; }
+    public bool NSFW { get; set; }
+    public double prijs { get; set; }
+}
---
+++ WebApplication1/Pages/Search.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Dapper;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualBasic;
+using MySql.Data.MySqlClient;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages
+{
+    public class Search : PageModel
+    {
+        public void OnGet()
+        {
+            var connection = new MySqlConnection(
+                "Server=127.0.0.1;Database=stripboekendb;Uid=root;Pwd=Test123;");
+            connection.Open();
+            
+            Stripboeken = connection.Query<Uitgave>("SELECT * FROM uitgave");
+            connection.Close();
+        }
+
+        public IEnumerable<Uitgave> Stripboeken { get; set; }
+        }
+    
+}

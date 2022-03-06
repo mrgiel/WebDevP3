5e42f5bb40164cba97460df72f79e6a99ce04c4f
BardoBardo <nigelhois50@gmail.com>
2022-03-06 18:32:21 +0100

    QueryOmOverzichtTeCreeren voor documenatie

---
+++ WebApplication1/Models/Bezit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Bezit
+    {
+        public int rating { get; }
+        
+        [Required]
+        public string staat { get; set; }
+
+        public string beschrijving { get; set; }
+
+        public int hoeveelheid { get; set; }
+        
+        [DataType(DataType.Currency)]
+        public float prijs_betaald { get; set; }
+        
+        public string gebruikersnaam { get; set; }
+
+        public int id { get; set; }
+
+    }
+}
---
+++ WebApplication1/Pages/QueryOmOverzichtTeCreeren.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Models;
+using WebApplication1.Repositories;
+
+namespace WebApplication1.Pages
+{
+    public class QueryOmOverzichtTeCreeren : PageModel
+    {
+        public IEnumerable<Bezit> data { get; set; }
+
+        public void OnGet()
+        {
+            data = new QueryOmOverzichtTeCreerenRepository().HaalCollectieOp();
+        }
+    }
+}

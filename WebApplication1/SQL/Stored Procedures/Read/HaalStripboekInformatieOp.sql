99ad913f954e575a9f9f0e1d97d810afbe392908
Ov8tg8g5ft7tc7tv7tv7uvg7tvtv7tv7H <nigelhois50@gmail.com>
2022-03-16 18:05:09 +0100

    stored procedures

---
+++ WebApplication1/SQL/Stored Procedures/Read/HaalStripboekInformatieOp.sql
@@ -0,0 +1,12 @@
+﻿DROP PROCEDURE IF EXISTS HaalStripboekInformatieOp ;
+
+CREATE PROCEDURE HaalStripboekInformatieOp()
+BEGIN
+SELECT v.afbeelding_url, isbn, datum, druk, prijs, u.naam, beschrijving, c.cat_naam, r.reeks_naam, i.rol, p.voornaam, achternaam
+FROM Versie v
+INNER JOIN Uitgave u ON u.uitgave_id = v.uitgave_id
+INNER JOIN Categorie c ON c.cat_id = u.cat_id
+INNER JOIN Reeks r ON r.reeks_id = u.reeks_id
+INNER JOIN isgemaaktdoor i on v.Versie_id = i.versie_id
+INNER JOIN persoon p ON p.persoon_id = i.persoon_id;
+END

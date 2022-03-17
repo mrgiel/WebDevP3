3bbb4df11ad5c92864c9f4014460428d1d7e8734
mrgiel <47106654+mrgiel@users.noreply.github.com>
2022-03-18 00:32:14 +0100

    admin pagina toegevoegd. geeft een overzicht van alle stripboeken. optie om stripboek te verwijderen, nog wel een bug in.

---
+++ WebApplication1/Pages/AdminPagina.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualBasic;
+using MySql.Data.MySqlClient;
+using WebApplication1.Models;
+using WebApplication1.Repositories;
+
+namespace WebApplication1.Pages;
+
+public class AdminPagina : PageModel
+{
+    public IEnumerable<Uitgave> Stripboeken { get; set; }
+
+    public IActionResult OnPostVerwijderen(string id)
+    {
+        Stripboeken = new adminRepository().Verwijderen(id);
+        return RedirectToPage("AdminPagina");
+    }
+    public void OnGet()
+    {
+        Stripboeken = new adminRepository().Get();
+    }
+
+
+}
--- WebApplication1/Repositories/adminRepository.cs
+++ WebApplication1/Repositories/adminRepository.cs
@@ -1,6 +1,32 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
+using WebApplication1.Models;
+
 namespace WebApplication1.Repositories;
 
-public class adminRepository
+public class adminRepository : DbConnection
 {
-    
+    public List<Uitgave> Get()
+    {
+        using IDbConnection verbinding = Connect();
+        var stripboeken = verbinding
+            .Query<Uitgave>(@"SELECT * 
+                             FROM uitgave uitgave 
+                             INNER JOIN versie versie on uitgave.uitgave_id = versie.uitgave_id
+                             INNER JOIN uitgever uitgever on versie.uitgever_id = uitgever.uitgever_id
+                             ORDER BY versie.isbn");
+                                
+        return stripboeken.ToList();
+    }
+
+    public IEnumerable<Uitgave> Verwijderen(string isbn)
+    {
+        using IDbConnection verbinding = Connect();
+        var stripboeken = verbinding
+            .Query<Uitgave>("DELETE FROM versie WHERE isbn = @Isbn",
+                new {Isbn = isbn, });
+        return stripboeken;
+    }
 }

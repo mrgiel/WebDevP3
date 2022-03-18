2292f32e51cddf06fc97c9fc7ba3fb59715fa811
Ov8tg8g5ft7tc7tv7tv7uvg7tvtv7tv7H <nigelhois50@gmail.com>
2022-03-18 12:36:16 +0100

    cleanup

--- WebApplication1/SQL/Stored Procedures/Update/idk.sql
+++ WebApplication1/SQL/Stored Procedures/Update/idk.sql
@@ -1,6 +1,20 @@
 ﻿DROP PROCEDURE IF EXISTS idk;
 
-CREATE PROCEDURE idk(Model varchar(100))
-BEGIN 
-INSERT INTO reeks(reeks_id,reeks_naam)VALUE(null,@Model) ;
+CREATE PROCEDURE idk(cat_naam varchar(100),
+#Uitgever
+    uitgever_naam varchar (100),
+    #Uitgave
+                     naam VARCHAR(100), hoogte INT, beschrijving VARCHAR(255),
+                     reeks_naam varchar(100))
+BEGIN
+    #Uitgave
+    INSERT INTO Uitgave(uitgave_id,naam, hoogte, beschrijving, cat_id, reeks_id)
+        SELECT null,@naam, @hoogte, @beschrijving, cat_id, reeks_id
+        FROM categorie,
+             reeks
+        WHERE naam NOT IN (
+            SELECT Uitgave.naam
+            FROM Uitgave
+        ) AND cat_naam = categorie.cat_naam
+          AND reeks_naam = reeks.reeks_naam;
 end;

cb0b140fa8b36ec7f86dd0e8d2c976386e1b0976
BardoBardo <nigelhois50@gmail.com>
2022-03-27 22:09:35 +0200

    persoon + isgemaakt door kan toevoegen

--- WebApplication1/Pages/StripboekToevoegen.cshtml.cs
+++ WebApplication1/Pages/StripboekToevoegen.cshtml.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Org.BouncyCastle.Crypto.Engines;
 using WebApplication1.Models.Klasse;
 using WebApplication1.Repositories;
 
@@ -18,21 +14,20 @@ namespace WebApplication1.Pages
         [BindProperty] public Versie Versie { get; init; } = new Versie();
 
         [BindProperty] public string PersoonArray { get; set; }
+        [BindProperty] public string RolArray { get; set; }
+        [BindProperty] public string AuteurArray { get; set; }
 
         public IActionResult OnPostSend()
         {
-            var a = PersoonArray.Split(new Char [] {'/' , ';' });
-            var b = string.Join(",",a[0]);
-            var c = string.Join(",",a[1]);
-            var d = PersoonArray != null ? (PersoonArray.Split(',').Length - 1) / 2 : 1;
-            var e = PersoonArray.Split(new Char [] {'/' , ';' })[0];
-            e = e;
-            
             //check
             if (!ModelState.IsValid) return Page();
             
+            var persoonArray = PersoonArray.Split('/');
+            var auteurArray = AuteurArray.Split('/');
+            var length = PersoonArray != null ? (PersoonArray.Split(',').Length - 1 + AuteurArray.Split(',').Length - 1) / 2 : 0;
+            
             //verstuur stripboeken
-            //new StripboekToevoegenRepository().VerstuurNieuwStripboek(Uitgave,Uitgave.Reeks ,Versie.Uitgever,Versie,Uitgave.Categorie,b,e,c,d,new GebruikerRepo().HaalIdOp());
+            new StripboekToevoegenRepository().VerstuurNieuwStripboek(Uitgave,Uitgave.Reeks ,Versie.Uitgever,Versie,Uitgave.Categorie,persoonArray.First() + auteurArray.First(),RolArray,persoonArray.Last() + auteurArray.Last(),length,new GebruikerRepo().HaalIdOp());
             return Page();
         }
     }
